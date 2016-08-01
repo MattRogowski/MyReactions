5e7661cd2ccb5ade27e8c833fad7fe0f84bcedfc
Matt Rogowski <mattrogowski@Matt-Rogowskis-Mackbook-Pro.local>
2016-08-01 20:11:57 +0100

    Initial demo

--- files/inc/plugins/myreactions.php
+++ files/inc/plugins/myreactions.php
@@ -264,6 +264,7 @@ function myreactions_postbit(&$post)
   background: #f5f5f5;
   border: 1px solid #ccc;
   display: inline-block;
+  border-radius: 6px;
 }
 .myreactions-reaction span {
   float: right;
